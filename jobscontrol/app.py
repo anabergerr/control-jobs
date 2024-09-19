88e15a0cd1d5e9573276a4f79c1b5b0acfa0288b
Otavio Saraiva <otavio.saraivax@gmail.com>
2024-09-18 23:02:25 -0300

    feat: add route get jobs

--- jobscontrol/app.py
+++ jobscontrol/app.py
@@ -2,6 +2,7 @@ from flask import Flask
 from config import Config
 from app.models import db, Job
 from app.routes.job_create import bp as routes_bp
+from app.routes.get_job import bp as get_bp
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -12,6 +13,8 @@ with app.app_context():
     db.create_all()
 
 app.register_blueprint(routes_bp)
+app.register_blueprint(get_bp)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
